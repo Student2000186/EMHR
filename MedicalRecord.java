428139daae5688a00131fcca2ab0e73376f09ea3
Student2000186 <rohancedmond@students.utech.edu.jm>
2026-03-26 18:37:45 -0500

    Enhance MedicalRecord with purposeForProcessing
    Added purposeForProcessing field and corresponding getter methods.

--- MedicalRecord.java
+++ MedicalRecord.java
@@ -1,15 +1,28 @@
 package medicalRecord;
 
 public abstract class MedicalRecord {
-	
-	  protected String patientID;
-	    protected String date;
 
-	    public MedicalRecord(String patientID,String date){
-	        this.patientID = patientID;
-	        this.date = date;
-	    }
+    protected String patientID;
+    protected String date;
+    protected String purposeForProcessing;
 
-	    public abstract void display();
+    public MedicalRecord(String patientID, String date, String purposeForProcessing) {
+        this.patientID = patientID;
+        this.date = date;
+        this.purposeForProcessing = purposeForProcessing;
+    }
 
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPurposeForProcessing() {
+        return purposeForProcessing;
+    }
+
+    public abstract void display();
 }
