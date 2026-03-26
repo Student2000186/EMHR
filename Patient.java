96f49d6cb2a46ce5136e76194da1f920edf80d08
Student2000186 <rohancedmond@students.utech.edu.jm>
2026-03-26 18:42:10 -0500

    Implement file I/O methods in Patient class
    Added methods for file serialization and deserialization.

--- Patient.java
+++ Patient.java
@@ -4,54 +4,58 @@ import person.Person;
 
 public class Patient extends Person {
 
-
-    // Stores patient contact number
     private String contact;
-
-    // Stores next of kin information
     private String nextOfKin;
 
-    public Patient()
-    {
-    	
-    	super();
+    public Patient() {
+        super();
     }
-    
-    // Constructor used when creating a new patient
+
     public Patient(String name, String id, String contact, String nextOfKin) {
-        super(name,id); // send shared attributes to Person
+        super(name, id);
         this.contact = contact;
         this.nextOfKin = nextOfKin;
     }
 
-    
-    //
-    public void setContact(String contact) {
-		this.contact = contact;
-	}
-    
-    // Returns contact
     public String getContact() {
         return contact;
     }
-     
-    
-    //
-	public void setNextOfKin(String nextOfKin) {
-		this.nextOfKin = nextOfKin;
-	}
 
-	
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 
-    // Returns next of kin
     public String getNextOfKin() {
         return nextOfKin;
     }
 
-    // Displays patient information
+    public void setNextOfKin(String nextOfKin) {
+        this.nextOfKin = nextOfKin;
+    }
+
+    public String toFileString() {
+        return escape(getId()) + "|" + escape(getName()) + "|" + escape(contact) + "|" + escape(nextOfKin);
+    }
+
+    public static Patient fromFileString(String line) {
+        String[] parts = line.split("\\|", -1);
+        if (parts.length != 4) {
+            return null;
+        }
+        return new Patient(unescape(parts[1]), unescape(parts[0]), unescape(parts[2]), unescape(parts[3]));
+    }
+
+    private static String escape(String value) {
+        return value == null ? "" : value.replace("|", "/");
+    }
+
+    private static String unescape(String value) {
+        return value == null ? "" : value;
+    }
+
     @Override
     public void display() {
-    	super.display();// display inherited Person details
+        super.display();
         System.out.println("Contact: " + contact);
         System.out.println("Next of Kin: " + nextOfKin);
     }
