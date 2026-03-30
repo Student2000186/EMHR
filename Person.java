857aea961c24d70c78d24fcd8b7bf1abd003258f
Student2000186 <rohancedmond@students.utech.edu.jm>
2026-03-29 22:05:58 -0500

    Add setId method and improve display format
    Added setId method and enhanced display output.

--- Person.java
+++ Person.java
@@ -1,3 +1,9 @@
+//EMHR Group PROJECT//
+// Rohan Edmond 2000186
+// Ryan Cole	2106111
+// Malachi-Shavario Shouta	2404920
+// Trev-ann Cameron	2405379
+//Gabriel Francis	2205720
 package person;
 
 public class Person {
@@ -5,29 +11,35 @@ public class Person {
     private String name;
     private String id;
 
-    public Person() {}
+    public Person() {
+    }
 
-    public Person(String name, String id){
+    public Person(String name, String id) {
         this.name = name;
         this.id = id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getId(){
+    public String getId() {
         return id;
     }
 
-    public void display(){
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void display() {
+        System.out.println("\n===== Display =====");
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
     }
 }
-   
+
 
